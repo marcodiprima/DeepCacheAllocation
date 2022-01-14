96e5751d44f53fbf4f20c4320dd312e8b3510a41
marcodiprima <marcodipri00@gmail.com>
2022-01-14 12:36:49 +0100

    new update

--- DeepCacheAllocation.py
+++ DeepCacheAllocation.py
@@ -4,40 +4,50 @@ import gym
 from gym import spaces
 
 MAX_CACHE_CAPACITY = 30  # cache capacity - limited to 30
-INITIAL_X1_SLOT = 15  #
+
 
 
 class DeepCacheNetw(gym.Env):
     """Deep QN Environment for Cache Allocation"""
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, x1):
+    def __init__(self, initial_x1):
         super(DeepCacheNetw, self).__init__()
-        self.x1 = x1
-        self.x2 = 30 - x1
+        # fixed allocation of cache resources to SPs
+        self.x1 = initial_x1  # SP1
+        self.x2 = MAX_CACHE_CAPACITY - initial_x1  # SP2
         self.reward_range = (-20, 0)  # -20 the worst case of all requests
-        self.action_space = spaces.Discrete(3)
+        self.action_space = spaces.Discrete(3)  # -1 0 1 action space range
+
+        print("x1 ", self.x1)
+        print("x2 ", self.x2)
 
         # The range in which x1 can take value
         self.observation_space = spaces.Box(
-            low=-20, high=0, shape=(1, 30), dtype=np.float16)
+            low=self.reward_range[0], high=self.reward_range[1], shape=(1, 30), dtype=np.float16)
 
-    # first count the cost, and then calculate the reward (that is the minus cost)
-    def step(self, action):
+    # first count the cost, and then calculate the reward (that is the opposite of the cost)
+    def step(self, action_before_conversion):
 
+        action = action_before_conversion - 1
+
+        #initialization
         penalty = 0
 
         # if the value of x1 + *action it takes* is negative or over the max_cache_capacity it gains a penalty
-        if self.x1 + action < 0 or self.x2 - action < 0 or self.x1 + action > MAX_CACHE_CAPACITY:
-            penalty = 300
+        if self.x1 + action < 0 or self.x2 - action < 0 or self.x1 + action > MAX_CACHE_CAPACITY or self.x2 - action > MAX_CACHE_CAPACITY:
+            penalty = 10
+            print("illegal action ", action)
+            print("x1 + action: ", self.x1+action)
+            print("x2 - action: ", self.x2-action)
         else:
             self.x1 = self.x1 + action
             self.x2 = self.x2 - action
+            print("legal action ", action)
 
         # Now, let's calculate the reward
-
-        rand_reqSP1 = np.random.randint(low=1, high=100, size=1)  # 10 random requests of sp1 (numbers from 1 to 100)
-        rand_reqSP2 = np.random.randint(low=1, high=200, size=100)  # 10 random requests of sp2 (numbers from 1 to 200)
+        rand_reqSP1 = np.random.randint(low=1, high=100, size=10)  # 10 random requests of sp1 (numbers from 1 to 100)
+        rand_reqSP2 = np.random.randint(low=1, high=100, size=10)  # 10 random requests of sp2 (numbers from 1 to 200)
 
         # everytime the requests is not in cache we increase the size of the instantaneous cost
 
@@ -55,14 +65,17 @@ class DeepCacheNetw(gym.Env):
 
         observation = self.x1
 
-        print('observation, reward = ', observation, reward)  # stamps every step x1 and the reward
+        print('x1, reward , x2 = ', observation, reward, self.x2)  # print for every step x1 and the reward
 
+        # necessary parameter to return
+        done = self.x1 <= 0 or self.x2 <= 0
 
-        return observation, reward, True, {}
+        return observation, reward, done, {}
 
     def reset(self):
         # Reset the state of the environment to an initial state
-        self.x1 = INITIAL_X1_SLOT
+        self.x1 = 3
+        self.x2 = MAX_CACHE_CAPACITY - self.x1
 
         observation = self.x1
 
--- main.py
+++ main.py
@@ -6,20 +6,18 @@ from DeepCacheAllocation import DeepCacheNetw
 
 import ctypes
 
-
 hllDll = ctypes.WinDLL("C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.5\\bin\\cudart64_100.dll")
 
 # The algorithms require a vectorized environment to run
-env = DummyVecEnv([lambda: DeepCacheNetw(0)])
+env = DummyVecEnv([lambda: DeepCacheNetw(25)])
 
 model = PPO('MlpPolicy', env, verbose=1)
 print("STARTING TO LEARN")
-model.learn(total_timesteps=20000, reset_num_timesteps=True)
+model.learn(total_timesteps=5000)
 
 print("STARTING TO PREDICT")
 obs = env.reset()
 for i in range(100):
     action, _states = model.predict(obs, deterministic=True)
     obs, rewards, done, info = env.step(action)
-    if done:
-        obs = env.reset()
+
