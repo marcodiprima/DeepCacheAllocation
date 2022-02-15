e160b999bc26ba40d2d708eb79e072734e62c8a8
marcodiprima <marcodipri00@gmail.com>
2022-02-15 12:27:30 +0100

    last update before modifications

--- DeepCacheAllocation.py
+++ DeepCacheAllocation.py
@@ -3,18 +3,24 @@ import numpy as np
 import gym
 from gym import spaces
 
-MAX_CACHE_CAPACITY = 30  # cache capacity - limited to 30
+import pandas as pd
+import matplotlib.pyplot as plt
+
+MAX_CACHE_CAPACITY = 100  # cache capacity - limited to 30
 
 class DeepCacheNetw(gym.Env):
     """Deep QN Environment for Cache Allocation"""
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, initial_x1):
+    def __init__(self, initial_x1, max_cost_SP1, max_cost_SP2):
         super(DeepCacheNetw, self).__init__()
         # fixed allocation of cache resources to SPs
+        self.initial_x1 = initial_x1
         self.x1 = initial_x1  # SP1
         self.x2 = MAX_CACHE_CAPACITY - initial_x1  # SP2
-        self.reward_range = (-20, 0)  # -20 the worst case of all requests
+        self.max_cost_SP1 = max_cost_SP1
+        self.max_cost_SP2 = max_cost_SP2
+        self.reward_range = (-max_cost_SP1-max_cost_SP2, 0)  # -20 the worst case of all requests
         self.action_space = spaces.Discrete(3,)  # -1 0 1 action space range
 
         print("x1 ", self.x1)
@@ -22,19 +28,22 @@ class DeepCacheNetw(gym.Env):
 
         # The range in which x1 can take value
         self.observation_space = spaces.Box(
-            low=self.reward_range[0], high=self.reward_range[1], shape=(1, 30), dtype=np.float16)
+            low=self.reward_range[0], high=self.reward_range[1], shape=(1, MAX_CACHE_CAPACITY), dtype=np.float16)
 
     # first count the cost, and then calculate the reward (that is the opposite of the cost)
     def step(self, action_before_conversion):
 
-        action = action_before_conversion - 1
+        print(" ")
+        action = 10*(action_before_conversion - 1)
 
-        #initialization
+        # initialization
         penalty = 0
+
+        print("x1 + action", self.x1 + action)
 
         # if the value of x1 + *action it takes* is negative or over the max_cache_capacity it gains a penalty
         if self.x1 + action < 0 or self.x2 - action < 0 or self.x1 + action > MAX_CACHE_CAPACITY or self.x2 - action > MAX_CACHE_CAPACITY:
-            penalty = 10
+            penalty = (self.max_cost_SP1+self.max_cost_SP2)/500
             print("illegal action ", action)
             print("x1 + action: ", self.x1+action)
             print("x2 - action: ", self.x2-action)
@@ -44,8 +53,13 @@ class DeepCacheNetw(gym.Env):
             print("legal action ", action)
 
         # Now, let's calculate the reward
-        rand_reqSP1 = np.random.randint(low=1, high=100, size=20)  # 10 random requests of sp1 (numbers from 1 to 100)
-        rand_reqSP2 = np.random.randint(low=1, high=100, size=90)  # 10 random requests of sp2 (numbers from 1 to 200)
+        #rand_reqSP1 = np.random.randint(low=1, high=100, size=self.max_cost_SP1)  # random requests of sp1 (numbers from 1 to 100)
+        #rand_reqSP2 = np.random.randint(low=1, high=100, size=self.max_cost_SP2)  # random requests of sp2 (numbers from 1 to 200)
+
+        #try lognormal
+
+        rand_reqSP1 = np.random.zipf(2.5, self.max_cost_SP1)
+        rand_reqSP2 = np.random.zipf(1.2, self.max_cost_SP2)
 
         # everytime the requests is not in cache we increase the size of the instantaneous cost
         inst_cost = 0
@@ -65,13 +79,13 @@ class DeepCacheNetw(gym.Env):
         print('x1, reward , x2 = ', observation, reward, self.x2)  # print for every step x1 and the reward
 
         # necessary parameter to return
-        done = self.x1 <= 0 or self.x2 <= 0
+        done = False
 
         return observation, reward, done, {}
 
     def reset(self):
         # Reset the state of the environment to an initial state
-        self.x1 = 15
+        self.x1 = self.initial_x1
         self.x2 = MAX_CACHE_CAPACITY - self.x1
 
         observation = self.x1
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3 import PPO
 
@@ -8,23 +9,43 @@ import ctypes
 
 hllDll = ctypes.WinDLL("C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\v11.5\\bin\\cudart64_100.dll")
 
+max_cost_SP1 = 200
+max_cost_SP2 = 300
+
 # The algorithms require a vectorized environment to run
-env = DummyVecEnv([lambda: DeepCacheNetw(25)])
+env = DummyVecEnv([lambda: DeepCacheNetw(90, max_cost_SP1, max_cost_SP2)])
 
 model = PPO('MlpPolicy', env, verbose=1)
 print("STARTING TO LEARN")
-model.learn(total_timesteps=5000)
-
-"""print("STARTING TO PREDICT")
-obs = env.reset()
-for i in range(100):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, rewards, done, info = env.step(action)
-"""
+model.learn(total_timesteps=50000)
+#try again with 1.2 and 1.2 and the same timesteps
 print("STARTING TO PREDICT")
 obs = env.reset()
+
+observation_list = []
+reward_list = []
 for time_step in range(100):
     action, _states = model.predict(obs, deterministic=True)
     obs, rewards, done, info = env.step(action)
-    if done:
-        obs = env.reset()
+    # making SP1 plot
+    observation_list.append(obs[0][0][0])
+    # making reward plot
+    reward_list.append(rewards[0])
+
+    with open("x1.txt", "a") as o:
+        o.write("\n")
+        o.write("\n".join(str(x) for x in obs))
+        o.write("\n")
+
+    with open("reward.txt", "a") as o:
+        o.write("\n")
+        o.write("\n".join(str(x) for x in rewards))
+        o.write("\n")
+
+plt.scatter(observation_list, reward_list)
+
+# plt.rcParams.update({'figure.figsize': (10, 8), 'figure.dpi': 100})
+plt.title('SP1 and its reward')
+plt.xlabel('SP1- value')
+plt.ylabel('REWARD - value')
+plt.show()
